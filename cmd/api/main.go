059981ec2e2b86014721f9c5d8a1e756cd16f54e
wasinphand <wasinph@outlook.com>
2024-05-06 12:58:56 +0700

    add .env and db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+
 	_ "github.com/NuttakitDW/gamification-platform/cmd/api/docs"
 	"github.com/NuttakitDW/gamification-platform/internal/handlers"
 	"github.com/gin-gonic/gin"
@@ -13,6 +21,14 @@ import (
 // @description This is a api server for gamification platform.
 // @BasePath /api/v1
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	initDatabase()
+
 	r := gin.New()
 
 	r.GET("/v1/api/helloworld/:name", handlers.GetStringByInt)
@@ -20,4 +36,25 @@ func main() {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run(":8080")
+}
+
+func initDatabase() {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	// Open a connection to the database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Test the database connection
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to the database!")
+
+
 }
--- go.mod
+++ go.mod
@@ -26,10 +26,12 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
